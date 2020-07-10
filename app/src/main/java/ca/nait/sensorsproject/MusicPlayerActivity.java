fd895d775e6c1ca81094716b8fde6abd949c8e11
Shams Ahsanullah <shamsahsan90@gmail.com>
2020-07-09 23:06:33 -0600

    Implemented features from the Audio Editor project

--- app/src/main/java/ca/nait/sensorsproject/MusicPlayerActivity.java
+++ app/src/main/java/ca/nait/sensorsproject/MusicPlayerActivity.java
@@ -3,6 +3,7 @@ package ca.nait.sensorsproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -11,6 +12,9 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -21,7 +25,9 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.lang.reflect.Field;
@@ -41,10 +47,24 @@ public class MusicPlayerActivity extends AppCompatActivity implements SensorEven
     private Sensor proximitySensor;
     private Boolean isProxSensorAvailable;
 
+    //Audio Editor implementation
+    SeekBar positionBar;
+    SeekBar volumeBar;
+    TextView elapsedTimeLabel;
+    TextView remainingTimeLabel;
+
+    float speed = 1.0f;
+    float pitch = 1.0f;
+    int totalTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
+
+        //Audio Editor implementation
+        elapsedTimeLabel = (TextView) findViewById(R.id.elapsedTimeLabel);
+        remainingTimeLabel = (TextView) findViewById(R.id.remainingTimeLabel);
 
         //sensor code
 
@@ -76,6 +96,34 @@ public class MusicPlayerActivity extends AppCompatActivity implements SensorEven
         mAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mArrayList);
         mListView.setAdapter(mAdapter);
 
+        //Audio editor implementation
+        volumeBar = (SeekBar) findViewById(R.id.volumeBar);
+        volumeBar.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        try{
+                            float volumeNum = progress / 100f;
+                            mMediaPlayer.setVolume(volumeNum, volumeNum);
+                        }
+                        catch(Exception e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+                    }
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+                    }
+                }
+        );
+        positionBar = (SeekBar) findViewById(R.id.positionBar);
+
+
         //listview onclick
 
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -92,11 +140,52 @@ public class MusicPlayerActivity extends AppCompatActivity implements SensorEven
                 int resId = getResources().getIdentifier(mArrayList.get(position),
                         "raw", getPackageName());
                 mMediaPlayer = MediaPlayer.create(MusicPlayerActivity.this, resId);
+
+
+                //Audio Editor implementation
+                mMediaPlayer.setLooping(true);
+                mMediaPlayer.seekTo(0);
+                mMediaPlayer.setVolume(0.5f, 0.5f);
+                mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(speed));
+                mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setPitch(pitch));
+                totalTime = mMediaPlayer.getDuration();
                 mMediaPlayer.start();
+
+                positionBar.setMax(totalTime);
+                positionBar.setOnSeekBarChangeListener(
+                        new SeekBar.OnSeekBarChangeListener() {
+                            @Override
+                            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                                if (fromUser) {
+                                    mMediaPlayer.seekTo(progress);
+                                    positionBar.setProgress(progress);
+                                }
+                            }
+                            @Override
+                            public void onStartTrackingTouch(SeekBar seekBar) {
+                            }
+                            @Override
+                            public void onStopTrackingTouch(SeekBar seekBar) {
+                            }
+                        }
+                );
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        while (mMediaPlayer != null) {
+                            try {
+                                Message msg = new Message();
+                                msg.what = mMediaPlayer.getCurrentPosition();
+                                handler.sendMessage(msg);
+                                Thread.sleep(1000);
+                            } catch (InterruptedException e) {}
+                        }
+                    }
+                }).start();
 
             }
         });
-
 
             imgPlay.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -158,6 +247,111 @@ public class MusicPlayerActivity extends AppCompatActivity implements SensorEven
                 SensorManager.SENSOR_DELAY_NORMAL);
 
     }
+
+
+
+    //audio editor implementation
+
+    private Handler handler = new Handler() {
+
+        @Override
+        public void handleMessage(Message msg) {
+            int currentPosition = msg.what;
+            // Update positionBar.
+            positionBar.setProgress(currentPosition);
+
+            // Update Labels.
+            String elapsedTime = createTimeLabel(currentPosition);
+            elapsedTimeLabel.setText(elapsedTime);
+
+            String remainingTime = createTimeLabel(totalTime-currentPosition);
+            remainingTimeLabel.setText("- " + remainingTime);
+        }
+    };
+
+    // Speed Buttons
+    public void speedDecreaseBtn(View view) {
+        try{
+            speed = speed - 0.15f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(speed));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public void speedIncreaseBtn(View view) {
+        try{
+            speed = speed + 0.15f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(speed));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public void speedResetBtn(View view) {
+        try{
+            speed = 1.0f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setSpeed(speed));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    // Pitch Buttons
+    public void pitchDecreaseBtn(View view) {
+        try{
+            pitch = pitch - 0.025f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setPitch(pitch));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public void pitchIncreaseBtn(View view) {
+        try{
+            pitch = pitch + 0.025f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setPitch(pitch));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+    public void pitchResetBtn(View view) {
+        try{
+            pitch = 1.0f;
+            mMediaPlayer.setPlaybackParams(mMediaPlayer.getPlaybackParams().setPitch(pitch));
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+
+    //audio editor implementation
+    public String createTimeLabel(int time) {
+        String timeLabel = "";
+        int min = time / 1000 / 60;
+        int sec = time / 1000 % 60;
+
+        timeLabel = min + ":";
+        if (sec < 10) timeLabel += "0";
+        timeLabel += sec;
+
+        return timeLabel;
+    }
+
 
     @Override
     public void onSensorChanged(final SensorEvent event)
